164abf17bd054a8387600670f95d4167d802faee
virxdxk <eliseevkonstantin306@gmail.com>
2026-02-11 11:14:01 +0300

    fix: swagger

--- src/main/java/ifmo/se/coursach_back/config/OpenApiConfig.java
+++ src/main/java/ifmo/se/coursach_back/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package ifmo.se.coursach_back.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,8 @@ import org.springframework.context.annotation.Configuration;
                 title = "Blood Donor Management System API",
                 version = "v1",
                 description = "REST API documentation for donor, medical, appointment, lab, and admin workflows."
-        )
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
 )
 @SecurityScheme(
         name = "bearerAuth",
